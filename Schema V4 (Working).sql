975f7551fca44f6b51c666581c9df1be16ff59b2
abderrahman504 <rahmankhsherb@gmail.com>
2023-01-04 14:47:49 +0200

    Latest working schema

---
+++ Schema V4 (Working).sql
@@ -0,0 +1,84 @@
+drop Schema BOOK_STORE;
+ CREATE SCHEMA BOOK_STORE;
+ use BOOK_STORE;
+ 
+ CREATE TABLE user_info(
+ userID INTEGER,
+ userName VARCHAR(50) ,
+ Password VARCHAR(100),
+ Lname VARCHAR(30),
+ Fname VARCHAR(20), 
+ Email VARCHAR(30),
+ Phone CHAR(11),
+ Address VARCHAR(100),
+ type VARCHAR(8),
+ primary key(userID)
+ );
+
+CREATE TABLE PUBLISHER(
+  Name varchar(50),
+  Address varchar(100),
+  Phone varchar(25),
+  PRIMARY KEY(Name)
+);
+ 
+ CREATE TABLE shopping_cart(
+ customerID INTEGER,
+ bookISBN INTEGER,
+ bookQuantity INTEGER
+ );
+ 
+ --  CREATE TABLE user_types(
+--  userName VARCHAR(50),
+--  type VARCHAR(8),
+--  
+--  );
+ 
+ CREATE TABLE BOOK (
+  ISBN INTEGER PRIMARY KEY,
+  Title varchar(30),
+  Author varchar(30),
+  Publisher_name varchar(30),
+  Year CHAR(4),
+  Selling_price float,
+  Category varchar(20),
+  foreign key (Publisher_name) references PUBLISHER (Name) on update cascade on delete set null
+  );
+  
+  
+ CREATE TABLE done_orders(
+ userID INTEGER,
+ bookISBN INTEGER,
+ bookQuantity INTEGER,
+ price FLOAT,
+ Date date,
+   foreign key (userID) references user_info (userID) on update cascade on delete set null,
+   foreign key (bookISBN) references book (ISBN) on update cascade on delete set null
+ );
+ 
+
+CREATE TABLE AUTHORS(
+  Book_id int,
+  Author_name varchar(50),
+  primary key (Author_name),
+  foreign key (Book_id) references BOOK(ISBN) on update cascade
+);
+
+
+CREATE TABLE BORROWER(
+  Card_no int,
+  Name varchar(50),
+  Address varchar(100),
+  Phone char(9),
+  PRIMARY KEY(Card_no)
+);
+
+CREATE TABLE BOOK_ORDER(
+  Order_id INTEGER,
+  customerID INTEGER,
+  Book_id int,
+  quantity int,
+  primary key(Order_id),
+  foreign key (Book_id) references BOOK(ISBN) on update cascade,
+  foreign key (customerID) references user_info(userID) on update cascade
+);
